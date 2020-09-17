f57a82fb52e4f705b3e65755cedfcdab4ea0f31d
absolutelyunique <59395978+absolutelyunique@users.noreply.github.com>
2020-09-16 22:33:54 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,20 @@
+package com.sonar.sample;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World !!");
+		System.out.println("Testing sonar qube !!");
+		
+		for(int i=1; i>=1; i++){
+            System.out.println("The value of i is: "+i);
+       }
+	   
+	   int j;
+		while (true) { // Noncompliant; end condition omitted
+		  j++;
+		}
+	}
+
+}
